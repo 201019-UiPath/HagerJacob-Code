b4f88d8a7575edd138323343004c7ee27fb37532
Jacob Hager <jslghager@gmail.com>
2020-10-20 09:26:18 -0500

    created helloworld and gitignore

---
+++ Csharp/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! " + DateTime.Now);
+            string name;
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name);
+
+            //alternate way to write using interpolation
+            Console.WriteLine($"Welcome {name}");
+        }
+    }
+}
